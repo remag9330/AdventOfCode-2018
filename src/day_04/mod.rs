c40f500f475a1a478dd406b60bd95f51e257513a
Brad <brad.crouch9330@gmail.com>
2018-12-09 17:29:45 +1100

    Day 4 skeleton

--- src/day_04/mod.rs
+++ src/day_04/mod.rs
@@ -1,16 +1,26 @@
 use util;
 
 pub fn run_part_1(args: &[String]) {
-    util::run_part_n("1", args, blah);
+    util::run_part_n("1", args, find_most_asleep_guard);
 }
 
-fn blah(filename: &String) -> util::AppResult {
-    let input = read_input(filename)?;
+fn find_most_asleep_guard(_filename: &String) -> util::AppResult {
+    // let input = read_input(filename)?;
 
     Ok(())
 }
 
-fn read_input(filename: &String) -> util::AppResult {
-    let input = util::read_file_input(filename)?;
-    Ok(())
-}
+// fn read_input(filename: &String) -> util::AppResult {
+//     let input = util::read_file_input(filename)?;
+//     let parsed = parse(&input.lines());
+//     Ok(())
+// }
+
+// fn parse(input: &Iterator<Item = &str>) -> i32 {
+//     0
+// }
+
+// struct Day {
+//     guard_id: String,
+
+// }
